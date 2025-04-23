f9d550499f3ed61c88942cbf1b7a1a1bedcfd179
alessandroibes <alessandro.ibes@gmail.com>
2025-04-23 13:11:36 -0300

    Alteração do calculo do dano usando a biblioteca random

--- jogo.py
+++ jogo.py
@@ -1,3 +1,5 @@
+import random
+
 # Personagem: classe mãe
 # Heroi: controlado pelo usuário
 # Inimigo: adversário do usuário
@@ -22,17 +24,19 @@ class Personagem:
     
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+
     
     def receber_ataque(self, dano):
         self.__vida -= dano
         if self.__vida < 0:
             self.__vida = 0
 
+
     def atacar(self, alvo):
-        dano = self.__nivel * 2
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4) # baseado no nível
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
-
+    
 
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
@@ -46,9 +50,10 @@ class Heroi(Personagem):
     
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+
     
     def ataque_especial(self, alvo):
-        dano = self.get_nivel() * 5 # Dano aumentado
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8) # Dano aumentado
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
 
@@ -65,7 +70,7 @@ class Inimigo(Personagem):
     
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n"
-
+    
 
 class Jogo():
     """ Classe orquestradora do jogo. """
@@ -73,6 +78,7 @@ class Jogo():
     def __init__(self) -> None:
         self.heroi = Heroi(nome="Heroi", vida=100, nivel=5, habilidade="Super Força")
         self.inimigo = Inimigo(nome="Morcego", vida=80, nivel=5, tipo="Voador")
+
 
     def iniciar_batalha(self):
         """ Fazer a gestão da batalha em turnos. """
@@ -84,8 +90,8 @@ class Jogo():
 
             input("Pressione Enter para atacar...")
             escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
-            
-            if escolha == '1':
+
+            if escolha == "1":
                 self.heroi.atacar(self.inimigo)
             elif escolha == "2":
                 self.heroi.ataque_especial(self.inimigo)
@@ -97,7 +103,7 @@ class Jogo():
                 self.inimigo.atacar(self.heroi)
 
         if self.heroi.get_vida() > 0:
-            print("\nParabéns você venceu a batalha!")
+            print("\nParabéns, você venceu a batalha!")
         else:
             print("\nVocê foi derrotado!")
 
